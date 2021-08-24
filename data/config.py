d5a308be9dd4c71dd9a3cb7788ca0325d4b69b56
Ilhom Jumayev <79079612+IlhomJumayevUrish@users.noreply.github.com>
2021-08-24 15:04:46 +0500

    make it better

--- data/config.py
+++ data/config.py
@@ -1,10 +1,15 @@
-from environs import Env
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+# Test varianti
+BOT_TOKEN = "1803705531:AAFKV4HN6zL0cVnGMfuUn2COeCL1vBmllts"  # Bot toekn
+ADMINS = ["732524345"]  # adminlar ro'yxati
+IP = "localhost"  # Xosting ip manzili
